5944c575ffd8d9ba460cdec8e53256c968247f1d
mysd33 <mysd.33@gmail.com>
2022-08-18 23:32:02 +0900

    SpringBatch実行対応

---
+++ sample-async/src/main/java/com/example/demo/app/message/AsyncMessageListener.java
@@ -0,0 +1,47 @@
+package com.example.demo.app.message;
+
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobInstanceAlreadyExistsException;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.support.JmsHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.domain.model.JobRequest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 
+ * キューを監視しジョブを実行するクラス
+ *
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AsyncMessageListener {
+	private final JobOperator jobOperator;
+	/**
+	 * キューからジョブの要求情報を受信
+	 * 
+	 * @param request ジョブの要求情報
+	 * @throws JobParametersInvalidException ジョブパラメータが不正
+	 * @throws NoSuchJobException 対象のジョブがない
+	 */
+	@JmsListener(destination =  "${aws.sqs.queue.name}")
+	public void onMessage(@Header(JmsHeaders.MESSAGE_ID)  final String messageId, final JobRequest request) 
+			throws JobParametersInvalidException, NoSuchJobException {
+			String jobId = request.getJobId();
+			String jobParameters = request.toParameterString();
+			log.info("ジョブ実行依頼受信[MessageId:{}][JobId:{}][JobParameter:{}]", messageId,jobId, jobParameters);			
+			try {
+				Long jobExecutionId = jobOperator.start(jobId, jobParameters);
+				log.info("ジョブ実行完了[MessageId:{}][JobId:{}][JobExecutionId:{}]", messageId, jobId, jobExecutionId);			
+			} catch (JobInstanceAlreadyExistsException e) {
+				log.warn("すでにこのジョブは実行されています。[SQS MessageId:{}][Jobid:{}][JobParameter:{}]", messageId, jobId, jobParameters);
+			}
+	}
+}
